1c9fb869ef942f9b87c64f7c168e89d9c3fb1d46
Кристина Нуритдинова <kvnuritdinova@edu.hse.ru>
2026-03-03 22:40:50 +0500

    добавлены тесты для функционала задания №1

--- tests/api/cats/test_get_cat.py
+++ tests/api/cats/test_get_cat.py
@@ -1,9 +1,39 @@
 import pytest
 import allure
 from tests.utils.data_builders import build_cat_payload, build_user_payload
+from utils.models import assert_cat_response
 import tests.utils.openapi_validator
 import logging
 logger = logging.getLogger(__name__)
+
+
+@pytest.mark.api
+@allure.feature("API")
+@allure.story("GET/cats/{id}")
+def test_get_cat_by_id(api):
+    logger.info("[API] Get cat by Id")
+
+    # Arrange 
+    payload = build_cat_payload()
+    
+    with allure.step("Создаём кота"):
+        logger.info(f"Создаём кота: {payload}")
+        create_resp = api.create_cat(payload)
+        cat_id = create_resp.json()["id"]
+        allure.attach(str(payload), name="created cat", attachment_type=allure.attachment_type.JSON)
+
+    # Act
+    with allure.step("Получаем созданного кота по его Id"):
+        logger.info(f"Получаем созданного кота по его Id: {cat_id}")
+        get_resp = api.get_cat_by_id(cat_id)
+        logger.debug(f"Найденный кот: {get_resp.json()}")
+        allure.attach(str(get_resp.json()), name="gotten cat", attachment_type=allure.attachment_type.JSON)
+
+    # Assert
+    with allure.step("Проверяем поля в ответе"):
+        logger.info("Проверяем поля в ответе")
+        assert_cat_response(get_resp.json(), payload["name"], payload["age"], payload["breed"], payload.get("history"), payload.get("description"))
+
 
 @pytest.mark.api
 @allure.feature("API")
