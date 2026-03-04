d41fb1baad65515b53f96333e79f8fa8cab91064
Uliana Degtiareva <uvdegtiareva@edu.hse.ru>
2026-03-04 23:38:11 +0500

    создание ветки без реализации для task 2

--- src/users/dto/update-user.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
-
-// PartialType автоматически делает все поля из CreateUserDto необязательными (@IsOptional)
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
