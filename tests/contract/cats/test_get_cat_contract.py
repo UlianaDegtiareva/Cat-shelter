07b997960cfbde5cab995486282476fd7b81b57d
Кристина Нуритдинова <kvnuritdinova@edu.hse.ru>
2026-03-05 18:20:37 +0500

    правки + ограничение на время выполнения

--- tests/contract/cats/test_get_cat_contract.py
+++ tests/contract/cats/test_get_cat_contract.py
@@ -36,9 +36,17 @@ def test_get_cat_by_Id_unauthorized_contract(api, openapi_validator, auth_token)
 @pytest.mark.contract
 @allure.feature("Contract")
 @allure.story("GET/cats without token")
-def test_get_all_cats_unauthorized_contract(api, openapi_validator):
+def test_get_all_cats_unauthorized_contract(api, openapi_validator, auth_token):
     logger.info("[GET ALL CATS][POSITIVE] do not required to be authorized")
     
+    # Arrange
+    cat_payload = build_cat_payload()
+    with allure.step(f"Создаем кота"):
+        logger.info(f"Создаем кота: {cat_payload}")
+        create_resp = api.create_cat(cat_payload, token=auth_token)
+        allure.attach(str(cat_payload), name="Cat", attachment_type=allure.attachment_type.JSON)
+    cat_id = create_resp.json()["id"]
+
     # Act
     with allure.step("Получениe всех котов без регистрации"):
         logger.info("Получениe всех котов без регистрации")
@@ -61,6 +69,7 @@ def test_get_all_cats_empty_contract(api, openapi_validator):
 
     # Act
     with allure.step("Запрашиваем всех котов в пустой БД"):
+        logger.info("Запрашиваем всех котов в пустой БД")
         get_resp = api.get_all_cats()
 
     # Assert
