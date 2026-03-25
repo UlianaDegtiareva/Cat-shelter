3e6777b8b941b1abff94ff9874e5943737970aaf
Кристина Нуритдинова <kvnuritdinova@edu.hse.ru>
2026-03-25 12:15:26 +0500

    демонстрация работы

--- tests/api/auth/test_login_api.py
+++ tests/api/auth/test_login_api.py
@@ -3,7 +3,7 @@ import allure
 from tests.utils.data_builders import build_user_payload
 import logging
 logger = logging.getLogger(__name__)
-
+#демонстрация работы
 @pytest.mark.api
 @allure.feature("API")
 @allure.story("POST/auth/login")
